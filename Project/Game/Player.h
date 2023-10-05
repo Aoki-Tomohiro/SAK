3ccf6330aa4694870d5ba313a1e9ac26875138a8
k022g0013@g.neec.ac.jp <k022g0013@g.neec.ac.jp>
2023-10-05 21:51:51 +0900

    Player

--- Project/Game/Player.h
+++ Project/Game/Player.h
@@ -1,5 +1,28 @@
 #pragma once
+#include "3D/Model/Model.h"
+#include "3D/Matrix/WorldTransform.h"
+#include "2D/ImGuiManager.h"
+#include "Components/Input.h"
+
 class Player
 {
+public:
+
+	void Initialize(Model* model, uint32_t textureHandle);
+
+	void Update();
+
+	void Draw(const ViewProjection viewProjection);
+
+private:
+	Input* input_ = nullptr;
+
+	Model* model_ = nullptr;
+
+	WorldTransform worldTransform_;
+
+	uint32_t textureHandle_ = 0u;
 };
+
+
 
