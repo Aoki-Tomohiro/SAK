c8a0c25c8791c5d0b59446c80e85028a60a20075
ShigaMitsuki <k022g0049@g.neec.ac.jp>
2023-10-23 19:04:27 +0900

    ClearSceneちょっといじった

--- Project/Game/ClearScene/GameClearScene.cpp
+++ Project/Game/ClearScene/GameClearScene.cpp
@@ -15,6 +15,28 @@ void GameClearScene::Initialize(GameManager* gameManager)
 	audio_ = Audio::GetInstance();
 	//Inputのインスタンスを取得
 	input_ = Input::GetInstance();
+
+	playerModel_.reset(Model::CreateFromOBJ("Resources/Platform", "Platform.obj"));
+	weaponModel_.reset(Model::CreateFromOBJ("Resources/Head", "Head.obj"));
+	bossModel_.reset(Model::CreateFromOBJ("Resources/Boss", "Boss.obj"));
+
+	playerWorldTransform_.translation_ = { 0.0f,-1.7f,2.0f };
+	playerWorldTransform_.scale_ = { 1.0f,1.0f,1.0f };
+
+	weaponWorldTransform_.translation_ = { 0.0f,-0.8f,2.0f };
+	weaponWorldTransform_.scale_ = { 1.0f,1.0f,1.0f };
+
+	bossWorldTransform_.translation_ = { 0.0f,-3.5f,2.0f };
+	bossWorldTransform_.rotation_.x = 1.35f;
+	bossWorldTransform_.scale_ = { 1.0f,1.0f,1.0f };
+
+	//背景の生成
+	backGround_ = std::make_unique<BackGround>();
+	backGround_->Initialize();
+
+	viewProjection_.UpdateMatrix();
+
+
 	//スプライトの生成
 	transitionSprite_.reset(Sprite::Create(transitionTextureHandle_, { 0.0f,0.0f }));
 	transitionSprite_->SetColor(transitionColor_);
@@ -23,6 +45,51 @@ void GameClearScene::Initialize(GameManager* gameManager)
 
 void GameClearScene::Update(GameManager* gameManager)
 {
+	//背景の更新
+	backGround_->Update();
+
+	float minusSpeed = 4.0f;
+
+	if (tMax <= t) {
+
+		t = 0;
+		if(isPush == true) {
+			isPush = false;
+		}
+	else {
+			isPush = true;
+		}
+	}
+
+	t++;
+
+	if (isPush == true) {
+		if (plus < plusMax) {
+			plus += minusSpeed;
+		}
+
+		if (plus >= plusMax) {
+			plus = plusMax;
+		}
+	}else if (isPush == false) {
+		if (plus > 0.0f) {
+			plus -= minusSpeed;
+		}
+
+		if (plus <= 0.0f) {
+			plus = 0.0f;
+		}
+	}
+
+	weaponWorldTransform_.translation_ = { 0.0f,-0.8f + plus,2.0f };
+	float ratio = plus / plusMax;
+
+	weaponWorldTransform_.scale_.x = 1.0f + 0.5f * (1.0f  - ratio);
+	weaponWorldTransform_.scale_.y = 1.0f + 0.5f * ratio;
+
+	playerWorldTransform_.scale_.x = 1.0f + 0.5f * (1.0f - ratio);
+	playerWorldTransform_.scale_.y = 1.0f + 0.5f * ratio;
+
 	if (input_->IsPushKeyEnter(DIK_SPACE))
 	{
 		if (isTransitionEnd_) {
@@ -54,12 +121,27 @@ void GameClearScene::Update(GameManager* gameManager)
 	ImGui::Begin("Game Clear");
 	ImGui::Text("push SPACE : Title");
 	ImGui::End();
+
+	playerWorldTransform_.UpdateMatrix();
+	weaponWorldTransform_.UpdateMatrix();
+	bossWorldTransform_.UpdateMatrix();
+
+	viewProjection_.UpdateMatrix();
 };
 
 void GameClearScene::Draw(GameManager* gameManager)
 {
 	//モデルの描画
 	Model::PreDraw();
+
+	//背景の描画
+	backGround_->Draw(viewProjection_);
+
+	playerModel_->Draw(playerWorldTransform_, viewProjection_);
+
+	weaponModel_->Draw(weaponWorldTransform_, viewProjection_);
+
+	bossModel_->Draw(bossWorldTransform_, viewProjection_);
 
 
 	Model::PostDraw();
