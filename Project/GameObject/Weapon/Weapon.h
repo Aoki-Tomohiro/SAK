ead6a5cae13f248c214363804146184c95077ee7
Aoki-Tomohiro <115062938+Aoki-Tomohiro@users.noreply.github.com>
2023-10-26 00:48:36 +0900

    ボスの撃破モーション中にダメージを食らわないようにする処理を追加

--- Project/GameObject/Weapon/Weapon.h
+++ Project/GameObject/Weapon/Weapon.h
@@ -54,6 +54,8 @@ public:
 
 	void StartAnimaion();
 
+	void GetBossIsDead(bool IsDeadAnimation) { IsDeadAnimation_ = IsDeadAnimation; };
+
 private:
 	Input* input_ = nullptr;
 
@@ -165,6 +167,8 @@ private:
 	std::unique_ptr<ParticleSystem> particleSystem_ = nullptr;
 
 	bool animationFlag_ = true;
+
+	bool IsDeadAnimation_ = false;
 
 };
 
